576c9bc8519e28203cccdaab24afa7c6a0829606
Courtney Woolum <117998701+serenefiresiren@users.noreply.github.com>
2024-11-02 18:52:00 -0400

    Index management (#4)
    * New index detail scripts, supports index optimization presentation.
    
    * New index assistants
    
    * Simplify index details
    Fix multi-column FK handling.
    
    ---------
    
    Co-authored-by: Courtney Woolum <cwoolum@i.pointeblank.net>
    Co-authored-by: cwoolum <cwoolum@pointeblank.net>

--- DefinitionManagement/ToggleForeignKeys.sql
+++ DefinitionManagement/ToggleForeignKeys.sql
@@ -1,37 +1,86 @@
-DECLARE @FilterTable nvarchar(255) = '[schema].[table]'
-
-SELECT CONCAT ('ALTER TABLE ', SourceTable, ' DROP CONSTRAINT ', ConstraintName) [DropConstraint]
-	, CONCAT ('ALTER TABLE ', SourceTable, ' NOCHECK CONSTRAINT ', ConstraintName) [NoCheck]
-	, CONCAT ('ALTER TABLE ', SourceTable, ' WITH CHECK ADD CONSTRAINT ', ConstraintName, ' FOREIGN KEY (', SourceColumn, ') REFERENCES ', TargetTable, ' (', TargetColumn, ')') 
-		+ CASE 
-			WHEN DeleteAction <> 'NO_ACTION'
-				THEN ' ON DELETE ' + DeleteAction
-			ELSE ''
-			END + CASE 
-			WHEN UpdateAction <> 'NO_ACTION'
-				THEN ' ON UPDATE ' + UpdateAction
-			ELSE ''
-			END [CreateConstraint]
-	, CONCAT ('ALTER TABLE ', SourceTable, '  CHECK CONSTRAINT ', ConstraintName) [ReCheck]
-	, ConstraintName
-	, SourceTable
-	, SourceColumn
-	, TargetTable
-	, TargetColumn
+SELECT CONCAT (
+		'ALTER TABLE '
+		,SourceTable
+		,' DROP CONSTRAINT IF EXISTS '
+		,ConstraintName
+		) [DropConstraint]
+	,CONCAT (
+		'ALTER TABLE '
+		,SourceTable
+		,' NOCHECK CONSTRAINT '
+		,ConstraintName
+		) [NoCheck]
+	,CONCAT (
+		'ALTER TABLE '
+		,SourceTable
+		,' WITH CHECK ADD CONSTRAINT '
+		,ConstraintName
+		,' FOREIGN KEY ('
+		,SourceColumns
+		,') REFERENCES '
+		,TargetTable
+		,' ('
+		,TargetColumns
+		,')'
+		) + CASE 
+		WHEN DeleteAction <> 'NO_ACTION'
+			THEN ' ON DELETE ' + REPLACE(DeleteAction, 'SET_NULL', 'SET NULL')
+		ELSE ''
+		END + CASE 
+		WHEN UpdateAction <> 'NO_ACTION'
+			THEN ' ON UPDATE ' + REPLACE(UpdateAction, 'SET_NULL', 'SET NULL')
+		ELSE ''
+		END [CreateConstraint]
+	,CONCAT (
+		'ALTER TABLE '
+		,SourceTable
+		,'  CHECK CONSTRAINT '
+		,ConstraintName
+		) [ReCheck]
+	,ConstraintName
+	,SourceTable
+	,SourceColumns
+	,TargetTable
+	,TargetColumns 
 FROM (
-	SELECT CONCAT ('[', OBJECT_SCHEMA_NAME(f.parent_object_id), '].[', OBJECT_NAME(f.parent_object_id), ']') SourceTable
-			, CONCAT ('[', OBJECT_SCHEMA_NAME(f.referenced_object_id), '].[', OBJECT_NAME(f.referenced_object_id), ']') TargetTable
-			, QUOTENAME(f.name) [ConstraintName]
-			, QUOTENAME(pc.name) [SourceColumn]
-			, QUOTENAME(rc.Name) [TargetColumn] 
-			, delete_referential_action_desc COLLATE SQL_Latin1_General_CP1_CI_AS [DeleteAction]
-			, update_referential_action_desc COLLATE SQL_Latin1_General_CP1_CI_AS [UpdateAction]
-	FROM sys.foreign_keys f
-	INNER JOIN sys.foreign_key_columns fkc ON fkc.constraint_object_id = f.object_id
-	INNER JOIN sys.columns pc ON fkc.parent_column_id = pc.column_id
-		AND fkc.parent_object_id = pc.object_id
-	INNER JOIN sys.columns rc ON fkc.referenced_column_id = rc.column_id
-		AND fkc.referenced_object_id = rc.object_id
-	) fd
-WHERE @FilterTable IS NULL OR SourceTable = @FilterTable OR TargetTable = @FilterTable
- 
+	SELECT CONCAT (
+			'['
+			,OBJECT_SCHEMA_NAME(fk.parent_object_id)
+			,'].['
+			,OBJECT_NAME(fk.parent_object_id)
+			,']'
+			) SourceTable
+		,CONCAT (
+			'['
+			,OBJECT_SCHEMA_NAME(fk.referenced_object_id)
+			,'].['
+			,OBJECT_NAME(fk.referenced_object_id)
+			,']'
+			) TargetTable
+		,QUOTENAME(fk.name) [ConstraintName]
+		,STRING_AGG(QUOTENAME(sc.name), ',') WITHIN
+	GROUP (
+			ORDER BY fkc.Constraint_Column_ID
+			) [SourceColumns]
+		,STRING_AGG(QUOTENAME(tc.Name), ',') WITHIN
+	GROUP (
+			ORDER BY fkc.Constraint_Column_ID
+			) [TargetColumns]
+		,delete_referential_action_desc COLLATE SQL_Latin1_General_CP1_CI_AS [DeleteAction]
+		,update_referential_action_desc COLLATE SQL_Latin1_General_CP1_CI_AS [UpdateAction] 
+	FROM sys.foreign_keys fk
+	INNER JOIN sys.foreign_key_columns fkc ON fkc.constraint_object_id = fk.object_id
+	INNER JOIN sys.columns sc ON fkc.parent_column_id = sc.column_id
+		AND fkc.parent_object_id = sc.object_id
+	INNER JOIN sys.columns tc ON fkc.referenced_column_id = tc.column_id
+		AND fkc.referenced_object_id = tc.object_id
+	GROUP BY fk.parent_object_id
+		,fk.referenced_object_id
+		,fk.name
+		,delete_referential_action_desc
+		,update_referential_action_desc
+		,fkc.Constraint_object_ID
+		,fk.object_id
+	) fkdtl
+ORDER BY DropConstraint
+
--- Index Details.sql
+++ Index Details.sql
@@ -1,43 +1,56 @@
-SELECT
-       SCHEMA_DATA.name AS schema_name,
-       TABLE_DATA.name AS table_name,
-       INDEX_DATA.name AS index_name,
-       STUFF((SELECT  ', ' + COLUMN_DATA_KEY_COLS.name + ' ' + CASE WHEN INDEX_COLUMN_DATA_KEY_COLS.is_descending_key = 1 THEN 'DESC' ELSE 'ASC' END -- Include column order (ASC / DESC)
-                FROM    sys.tables AS T
-                        INNER JOIN sys.indexes INDEX_DATA_KEY_COLS
-                                         ON T.object_id = INDEX_DATA_KEY_COLS.object_id
-                        INNER JOIN sys.index_columns INDEX_COLUMN_DATA_KEY_COLS
-                                         ON INDEX_DATA_KEY_COLS.object_id = INDEX_COLUMN_DATA_KEY_COLS.object_id
-                        AND INDEX_DATA_KEY_COLS.index_id = INDEX_COLUMN_DATA_KEY_COLS.index_id
-                        INNER JOIN sys.columns COLUMN_DATA_KEY_COLS
-                                         ON T.object_id = COLUMN_DATA_KEY_COLS.object_id
-                        AND INDEX_COLUMN_DATA_KEY_COLS.column_id = COLUMN_DATA_KEY_COLS.column_id
-                WHERE   INDEX_DATA.object_id = INDEX_DATA_KEY_COLS.object_id
-                        AND INDEX_DATA.index_id = INDEX_DATA_KEY_COLS.index_id
-                        AND INDEX_COLUMN_DATA_KEY_COLS.is_included_column = 0
-                ORDER BY INDEX_COLUMN_DATA_KEY_COLS.key_ordinal
-                FOR XML PATH('')), 1, 2, '') AS key_column_list ,
-   STUFF(( SELECT  ', ' + COLUMN_DATA_INC_COLS.name
-                FROM    sys.tables AS T
-                        INNER JOIN sys.indexes INDEX_DATA_INC_COLS
-                                         ON T.object_id = INDEX_DATA_INC_COLS.object_id
-                        INNER JOIN sys.index_columns INDEX_COLUMN_DATA_INC_COLS
-                                         ON INDEX_DATA_INC_COLS.object_id = INDEX_COLUMN_DATA_INC_COLS.object_id
-                        AND INDEX_DATA_INC_COLS.index_id = INDEX_COLUMN_DATA_INC_COLS.index_id
-                        INNER JOIN sys.columns COLUMN_DATA_INC_COLS
-                                         ON T.object_id = COLUMN_DATA_INC_COLS.object_id
-                        AND INDEX_COLUMN_DATA_INC_COLS.column_id = COLUMN_DATA_INC_COLS.column_id
-                WHERE   INDEX_DATA.object_id = INDEX_DATA_INC_COLS.object_id
-                        AND INDEX_DATA.index_id = INDEX_DATA_INC_COLS.index_id
-                        AND INDEX_COLUMN_DATA_INC_COLS.is_included_column = 1
-                ORDER BY INDEX_COLUMN_DATA_INC_COLS.key_ordinal
-                FOR XML PATH('')), 1, 2, '') AS include_column_list,
-       INDEX_DATA.is_disabled -- Check if index is disabled before determining which dupe to drop (if applicable)
-FROM sys.indexes INDEX_DATA
-INNER JOIN sys.tables TABLE_DATA
-ON TABLE_DATA.object_id = INDEX_DATA.object_id
-INNER JOIN sys.schemas SCHEMA_DATA
-ON SCHEMA_DATA.schema_id = TABLE_DATA.schema_id
-WHERE TABLE_DATA.is_ms_shipped = 0
-AND INDEX_DATA.type_desc IN ('NONCLUSTERED', 'CLUSTERED')
-order by 2, 3
+SELECT s.name AS schema_name
+	,t.name AS table_name
+	,i.name AS index_name
+	,i.IS_PRimary_Key [PK]
+	,CASE i.Type
+		WHEN 1
+			THEN 1
+		ELSE 0
+		END [CI]
+	,i.is_unique [UQ]
+	,STUFF((
+			SELECT ', ' + c.name + ' ' + CASE 
+					WHEN ic.is_descending_key = 1
+						THEN 'DESC'
+					ELSE 'ASC'
+					END
+			FROM sys.index_columns ic
+			INNER JOIN sys.columns c ON ic.object_id = c.object_id
+				AND ic.column_id = c.column_id
+			WHERE ic.object_id = i.object_id
+				AND ic.index_id = i.index_id
+				AND ic.is_included_column = 0
+			ORDER BY ic.key_ordinal
+			FOR XML PATH('')
+			), 1, 2, '') AS key_column_list
+	, 
+	STUFF((
+			SELECT ', ' + c.name
+			FROM sys.index_columns ic
+			INNER JOIN sys.columns c ON ic.object_id = c.object_id
+				AND ic.column_id = c.column_id
+			WHERE ic.object_id = i.object_id
+				AND ic.index_id = i.index_id
+				AND ic.is_included_column = 1
+			ORDER BY ic.key_ordinal
+			FOR XML PATH('')
+			), 1, 2, '') AS include_column_list
+	,i.filter_definition
+FROM sys.indexes i
+INNER JOIN sys.tables t ON t.object_id = i.object_id
+INNER JOIN sys.schemas s ON s.schema_id = t.schema_id
+WHERE t.is_ms_shipped = 0
+	AND i.type_desc IN (
+		'NONCLUSTERED'
+		,'CLUSTERED'
+		)
+ORDER BY schema_name
+	,table_name
+	,CASE 
+		WHEN i.is_primary_key = 1
+			THEN 1
+		WHEN i.Type = 1
+			THEN 1
+		END DESC
+	,i.is_primary_key
+	,index_name;
